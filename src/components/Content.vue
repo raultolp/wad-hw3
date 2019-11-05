32d1cd71c21bf98b01b14c9dff06caab60af7267
RenetRamman <renet.ramman@gmail.com>
2019-11-05 20:07:39 +0200

    Made the add-course button work?

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -62,13 +62,13 @@
                     <br>
                     <div>
                         <button id="add-course-button" class="blue-button" @click="isShowing ^= true">+</button>
-                        <span id="add-course">
-                                <input class="input" type="text" placeholder="Course title" id="title">
-                                <input class="input" type="number" min="1" max="8" placeholder="Semester" id="semester">
-                                <input class="input" type="number" min="0" max="100" placeholder="Grade" id="grade">
-                                <button class="green-button" id="save-course">Save</button>
-                                <button class="grey-button" id="cancel-course">Cancel</button>
-                            </span>
+                        <span v-show="isShowing" id="add-course">
+                            <input class="input" type="text" placeholder="Course title" id="title">
+                            <input class="input" type="number" min="1" max="8" placeholder="Semester" id="semester">
+                            <input class="input" type="number" min="0" max="100" placeholder="Grade" id="grade">
+                            <button class="green-button" id="save-course">Save</button>
+                            <button class="grey-button" id="cancel-course">Cancel</button>
+                        </span>
                     </div>
                 </div>
             </div>
@@ -192,7 +192,5 @@
         min-width: 135px;
     }
 
-    #add-course {
-        display: none;
-    }
+    /*#add-course {display: none;}*/
 </style>
